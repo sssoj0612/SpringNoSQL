a420ea66649bf09115b13d891d97b62b2d6a94d9
sojeong <sssoj0612@gmail.com>
2024-05-21 16:19:11 +0900

    annyang.js를 사용하여 마이크에 얘기한 내용을 텍스트로 변환하고 Redis DB에 저장

---
+++ src/main/java/kopo/poly/service/IMovieService.java
@@ -0,0 +1,12 @@
+package kopo.poly.service;
+
+import kopo.poly.dto.MovieDTO;
+
+import java.util.List;
+
+public interface IMovieService {
+
+    /* 영화 정보가져오기 */
+    List<MovieDTO> getMovieRank() throws Exception;
+
+}
